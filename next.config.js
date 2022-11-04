56f57ceb79b0831a84518d0eef7d765f9bf7490e
Zach Robert <zrobert@mojotech.com>
2022-11-04 10:17:02 -0400

    [feat] add scss stylelinting

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
+  // for Stylelint
+  webpack: (config, options) => {
+    config.plugins.push(new StylelintPlugin());
+    return config;
+  },
+
 }
 
 module.exports = nextConfig
