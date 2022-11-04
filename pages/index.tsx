8ebb3dbea689cec6166be8cead5f8a1cb4525a59
Zach Robert <zrobert@mojotech.com>
2022-11-04 09:19:05 -0400

    [feat] init nextjs app

---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+	const callAPI = async () => {
+		try {
+			const res = await fetch(
+				'https://api.abyiss.com/v1/ping'
+			)
+			const data = await res.json()
+			// console.log(data)
+		} catch (err) {
+			// console.log(err)
+		}
+	}
+	return (
+		<div className={styles.container}>
+			<main className={styles.main}>
+				<button onClick={callAPI}>Make API Call</button>
+			</main>
+		</div>
+	)
+}
