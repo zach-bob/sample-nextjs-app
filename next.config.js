a9e6a71c7bcd016cc720d2c22c36b859a3e710a3
Zach Robert <zrobert@mojotech.com>
2022-11-04 11:22:44 -0400

    [fix] nextjs config by removing webpack plugin

--- next.config.js
+++ next.config.js
@@ -2,13 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-
-  // for Stylelint
-  webpack: (config, options) => {
-    config.plugins.push(new StylelintPlugin());
-    return config;
-  },
-
 }
 
 module.exports = nextConfig
